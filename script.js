f02b66615a60c8cd80949302741a8793e24ca4b3
Bunkabito <lijan987@gmail.com>
2025-02-10 23:53:09 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Are you really really sure?",
+    "Puku Pleasee.....",
+    "Give it a thought puku",
+    "If you say no, I will start crying T_T",
+    "I will be sad puku :(((",
+    "I will be extremely sad my ichuuuu.... :[",
+    "Alright fine puku, I will stop asking so just click on me last time",
+    "Muhaahaha gotcha puku I was kidding now please say YES zinni. 💝"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
